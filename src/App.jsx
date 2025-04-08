26a82b189bb93dce2dc89f4aae55ad5708a41007
ezgikardes <ezgikardes@gmail.com>
2025-04-08 22:07:16 +0200

    feat: add AppNav component and integrate AppLayout in routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Product from "./pages/Product"
 import Pricing from "./pages/Pricing"
 import Homepage from "./pages/Homepage"
 import PageNotFound from "./pages/PageNotFound"
+import AppLayout from "./pages/AppLayout"
 
 function App() {
 
@@ -13,7 +14,8 @@ function App() {
           <Route path="/" element={<Homepage />} />
           <Route path="product" element={<Product />} />
           <Route path="pricing" element={<Pricing />} />
-          <Route path="*" element={PageNotFound} />
+          <Route path="app" element={<AppLayout />} />
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
